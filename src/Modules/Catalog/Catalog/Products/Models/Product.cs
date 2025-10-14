65603f8057560a4538883e27972e4d8823aa5d23
AbdulazizALNamshah <abdula2i2namsha@gmail.com>
2025-10-15 00:27:22 +0300

    Developing Catalog Domain with Product Domain Entity Models

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using Catalog.Products.Events;
+
+namespace Catalog.Products.Models;
+public class Product : Aggregate<Guid>
+{
+    public string Name { get; set; } = default!;
+    public List<string> Categories { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+    public static Product Create(string name, List<string> categories, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Name = name,
+            Categories = categories,
+            Description = description,
+            ImageFile = imageFile,
+            Price = price
+        };
+
+        product.AddDomainEvent(new ProductCreatedEvent(product));
+
+        return product;
+    }
+
+    public void Update(string name, List<string> categories, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        
+        Name= name;
+        Categories= categories;
+        Description= description;
+        ImageFile= imageFile;
+        Price= price;
+
+        if (Price != price)
+        {
+            Price = price;
+            AddDomainEvent(new ProductPriceChangedEvent(this));
+        }
+    }
+}
+
