fd1ac8ade3ce94aabd2f9f73d5caec8b00e02f2e
AbdulazizALNamshah <abdula2i2namsha@gmail.com>
2025-10-14 22:20:20 +0300

    Developing Shared Domain for DDD Abstraction (Entity, Aggreagate, Events)

---
+++ src/Shared/Shared/DDD/Aggregate.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.DDD;
+public abstract class Aggregate<TID> : Entity<TID>, IAggregate<TID>
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+        _domainEvents.Clear();
+        return dequeuedEvents;
+    }
+}
---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.DDD;
+public interface IDomainEvent:INotification
+{
+    public Guid EventId=> Guid.NewGuid();
+    public DateTime? OccurredOn=> DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName!;
+
+}
