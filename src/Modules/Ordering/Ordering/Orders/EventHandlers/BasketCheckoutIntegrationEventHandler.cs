47ca727e13de171e3c543d2391ea8ad77e4d5a65
AbdulazizALNamshah <abdula2i2namsha@gmail.com>
2025-11-05 23:59:29 +0300

    Developing Basket Checkout Event Consumer Integration Event Handler

---
+++ src/Modules/Ordering/Ordering/Orders/EventHandlers/BasketCheckoutIntegrationEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using MassTransit;
+using Ordering.Orders.Features.CreateOrder;
+using Shared.Messaging.Events;
+
+namespace Ordering.Orders.EventHandlers;
+
+public class BasketCheckoutIntegrationEventHandler(ISender sender, ILogger<BasketCheckoutIntegrationEventHandler> logger) : IConsumer<BasketCheckoutIntegrationEvent>
+{
+    public async Task Consume(ConsumeContext<BasketCheckoutIntegrationEvent> context)
+    {
+        logger.LogInformation("Integration Event handled : {IntegrationEvent}", context.Message.GetType().Name);
+
+        // Create new order and start order fullfillment process
+        var createOrderCommand = MapToCreateOrderCommand(context.Message);
+        await sender.Send(createOrderCommand);
+    }
+    private CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutIntegrationEvent message)
+    {
+        // Create full order with incoming event data
+        var addressDto = new AddressDto(message.FirstName, message.LastName, message.EmailAddress, message.AddressLine, message.Country, message.State, message.ZipCode);
+        var paymentDto = new PaymentDto(message.CardName, message.CardNumber, message.Expiration, message.Cvv, message.PaymentMethod);
+        var orderId = Guid.NewGuid();
+
+        var orderDto = new OrderDto(
+            Id: orderId,
+            CustomerId: message.CustomerId,
+            OrderName: message.UserName,
+            ShippingAddress: addressDto,
+            BillingAddress: addressDto,
+            Payment: paymentDto,
+            Items:
+            [
+                new OrderItemDto(orderId, new Guid("6e078cf2-3a28-4c7d-a32d-6add1c07a7c4"), 2, 1500),
+                new OrderItemDto(orderId, new Guid("a3b4c5d6-e7f8-4a9b-bc1d-2e3f4a5b6c7d"), 1, 1199)
+            ]);
+
+        return new CreateOrderCommand(orderDto);
+    }
+}
